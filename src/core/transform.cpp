869a3ecff44bccd8112b14dd1591a5ae179e4c5e
keptsecret <sorchon@gmail.com>
2022-07-02 10:59:52 +0700

    implemented transforms

---
+++ src/core/transform.cpp
@@ -0,0 +1,265 @@
+#include "transform.h"
+
+namespace civet {
+
+bool solveLinearSystem(const float A[2][2], const float B[2], float* x0, float* x1) {
+	float det = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+	if (std::abs(det) < 1e-10f) {
+		return false;
+	}
+	*x0 = (A[1][1] * B[0] - A[0][1] * B[1]) / det;
+	*x1 = (A[0][0] * B[1] - A[1][0] * B[0]) / det;
+	if (std::isnan(*x0) || std::isnan(*x1)) {
+		return false;
+	}
+	return true;
+}
+
+CIVET_CPU_GPU
+Matrix4::Matrix4(float mat[4][4]) {
+	memcpy(m, mat, 16 * sizeof(float));
+}
+
+CIVET_CPU_GPU
+Matrix4::Matrix4(float t00, float t01, float t02, float t03,
+		float t10, float t11, float t12, float t13,
+		float t20, float t21, float t22, float t23,
+		float t30, float t31, float t32, float t33) {
+	m[0][0] = t00;
+	m[0][1] = t01;
+	m[0][2] = t02;
+	m[0][3] = t03;
+	m[1][0] = t10;
+	m[1][1] = t11;
+	m[1][2] = t12;
+	m[1][3] = t13;
+	m[2][0] = t20;
+	m[2][1] = t21;
+	m[2][2] = t22;
+	m[2][3] = t23;
+	m[3][0] = t30;
+	m[3][1] = t31;
+	m[3][2] = t32;
+	m[3][3] = t33;
+}
+
+CIVET_CPU_GPU
+Matrix4 transpose(const Matrix4& mat) {
+	return Matrix4(mat.m[0][0], mat.m[1][0], mat.m[2][0], mat.m[3][0],
+			mat.m[0][1], mat.m[1][1], mat.m[2][1], mat.m[3][1],
+			mat.m[0][2], mat.m[1][2], mat.m[2][2], mat.m[3][2],
+			mat.m[0][3], mat.m[1][3], mat.m[2][3], mat.m[3][3]);
+}
+
+CIVET_CPU_GPU
+Matrix4 inverse(const Matrix4& mat) {
+	int indxc[4], indxr[4];
+	int ipiv[4] = { 0, 0, 0, 0 };
+	float minv[4][4];
+	memcpy(minv, mat.m, 4 * 4 * sizeof(float));
+	for (int i = 0; i < 4; i++) {
+		int irow = 0, icol = 0;
+		float big = 0.f;
+		// Choose pivot
+		for (int j = 0; j < 4; j++) {
+			if (ipiv[j] != 1) {
+				for (int k = 0; k < 4; k++) {
+					if (ipiv[k] == 0) {
+						if (std::abs(minv[j][k]) >= big) {
+							big = float(std::abs(minv[j][k]));
+							irow = j;
+							icol = k;
+						}
+					} else if (ipiv[k] > 1) {
+						printf("ERROR::Matrix4: Singular matrix in MatrixInvert\n");
+					}
+				}
+			}
+		}
+		++ipiv[icol];
+		// Swap rows _irow_ and _icol_ for pivot
+		if (irow != icol) {
+			for (int k = 0; k < 4; ++k) {
+				swap(minv[irow][k], minv[icol][k]);
+			}
+		}
+		indxr[i] = irow;
+		indxc[i] = icol;
+		if (minv[icol][icol] == 0.f) {
+			printf("ERROR::Matrix4: Singular matrix in MatrixInvert\n");
+		}
+
+		// Set $mat[icol][icol]$ to one by scaling row _icol_ appropriately
+		float pivinv = 1. / minv[icol][icol];
+		minv[icol][icol] = 1.;
+		for (int j = 0; j < 4; j++) {
+			minv[icol][j] *= pivinv;
+		}
+
+		// Subtract this row from others to zero out their columns
+		for (int j = 0; j < 4; j++) {
+			if (j != icol) {
+				float save = minv[j][icol];
+				minv[j][icol] = 0;
+				for (int k = 0; k < 4; k++) {
+					minv[j][k] -= minv[icol][k] * save;
+				}
+			}
+		}
+	}
+	// Swap columns to reflect permutation
+	for (int j = 3; j >= 0; j--) {
+		if (indxr[j] != indxc[j]) {
+			for (int k = 0; k < 4; k++) {
+				swap(minv[k][indxr[j]], minv[k][indxc[j]]);
+			}
+		}
+	}
+	return Matrix4(minv);
+}
+
+CIVET_CPU_GPU
+Transform translate(const Vector3f& delta) {
+	Matrix4 m(1, 0, 0, delta.x,
+			0, 1, 0, delta.y,
+			0, 0, 1, delta.z,
+			0, 0, 0, 1);
+	Matrix4 m_inv(1, 0, 0, -delta.x,
+			0, 1, 0, -delta.y,
+			0, 0, 1, -delta.z,
+			0, 0, 0, 1);
+	return Transform(m, m_inv);
+}
+
+CIVET_CPU_GPU
+Transform scale(const float x, const float y, const float z) {
+	Matrix4 m(x, 0, 0, 0,
+			0, y, 0, 0,
+			0, 0, z, 0,
+			0, 0, 0, 1);
+	Matrix4 m_inv(1 / x, 0, 0, 0,
+			0, 1 / y, 0, 0,
+			0, 0, 1 / z, 0,
+			0, 0, 0, 1);
+	return Transform(m, m_inv);
+}
+
+CIVET_CPU_GPU
+Transform rotateX(float theta) {
+	float sin_theta = std::sin(radians(theta));
+	float cos_theta = std::cos(radians(theta));
+	Matrix4 m(1, 0, 0, 0,
+			0, cos_theta, -sin_theta, 0,
+			0, sin_theta, cos_theta, 0,
+			0, 0, 0, 1);
+	return Transform(m, transpose(m));
+}
+
+CIVET_CPU_GPU
+Transform rotateY(float theta) {
+	float sin_theta = std::sin(radians(theta));
+	float cos_theta = std::cos(radians(theta));
+	Matrix4 m(cos_theta, 0, sin_theta, 0,
+			0, 1, 0, 0,
+			-sin_theta, 0, cos_theta, 0,
+			0, 0, 0, 1);
+	return Transform(m, transpose(m));
+}
+
+CIVET_CPU_GPU
+Transform rotateZ(float theta) {
+	float sin_theta = std::sin(radians(theta));
+	float cos_theta = std::cos(radians(theta));
+	Matrix4 m(cos_theta, -sin_theta, 0, 0,
+			0, sin_theta, cos_theta, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 1);
+	return Transform(m, transpose(m));
+}
+
+CIVET_CPU_GPU
+Transform rotate(float theta, Vector3f& axis) {
+	Vector3f a = normalize(axis);
+	float sin_theta = std::sin(radians(theta));
+	float cos_theta = std::cos(radians(theta));
+	Matrix4 m;
+
+	// rotation of first basis vector
+	m.m[0][0] = a.x * a.x + (1 - a.x * a.x) * cos_theta;
+	m.m[0][1] = a.x * a.y + (1 - cos_theta) - a.z * sin_theta;
+	m.m[0][2] = a.x * a.z + (1 - cos_theta) - a.y * sin_theta;
+	m.m[0][3] = 0;
+
+	// rotation of second basis vector
+	m.m[1][0] = a.x * a.y + (1 - cos_theta) - a.x * sin_theta;
+	m.m[1][1] = a.y * a.y + (1 - a.y * a.y) * cos_theta;
+	m.m[1][2] = a.y * a.z + (1 - cos_theta) - a.z * sin_theta;
+	m.m[1][3] = 0;
+
+	// rotation of third basis vector
+	m.m[2][0] = a.x * a.z + (1 - cos_theta) - a.x * sin_theta;
+	m.m[2][1] = a.y * a.z + (1 - cos_theta) - a.y * sin_theta;
+	m.m[2][2] = a.z * a.z + (1 - a.z * a.z) * cos_theta;
+	m.m[2][3] = 0;
+
+	return Transform(m, transpose(m));
+}
+
+CIVET_CPU_GPU
+Transform lookAt(const Point3f& position, const Point3f& target, const Vector3f& up) {
+	Matrix4 camera_to_world;
+	camera_to_world.m[0][3] = position.x;
+	camera_to_world.m[1][3] = position.y;
+	camera_to_world.m[2][3] = position.z;
+	camera_to_world.m[3][3] = 1;
+
+	Vector3f dir = normalize(target - position);
+	Vector3f right = normalize(cross(normalize(up), dir));
+	Vector3f new_up = normalize(cross(dir, right));
+
+	camera_to_world.m[0][0] = right.x;
+	camera_to_world.m[1][0] = right.y;
+	camera_to_world.m[2][0] = right.z;
+	camera_to_world.m[3][0] = 0;
+
+	camera_to_world.m[0][1] = new_up.x;
+	camera_to_world.m[1][1] = new_up.y;
+	camera_to_world.m[2][1] = new_up.z;
+	camera_to_world.m[3][1] = 0;
+
+	camera_to_world.m[0][2] = dir.x;
+	camera_to_world.m[1][2] = dir.y;
+	camera_to_world.m[2][2] = dir.z;
+	camera_to_world.m[3][2] = 0;
+
+	return Transform(inverse(camera_to_world), camera_to_world);
+}
+
+CIVET_CPU_GPU
+Bounds3f Transform::operator()(const Bounds3f& b) const {
+	const Transform& M = *this;
+	Bounds3f ret(M(Point3f(b.p_min.x, b.p_min.y, b.p_min.z)));
+	ret = bUnion(ret, M(Point3f(b.p_max.x, b.p_min.y, b.p_min.z)));
+	ret = bUnion(ret, M(Point3f(b.p_min.x, b.p_max.y, b.p_min.z)));
+	ret = bUnion(ret, M(Point3f(b.p_min.x, b.p_min.y, b.p_max.z)));
+	ret = bUnion(ret, M(Point3f(b.p_max.x, b.p_max.y, b.p_min.z)));
+	ret = bUnion(ret, M(Point3f(b.p_min.x, b.p_max.y, b.p_max.z)));
+	ret = bUnion(ret, M(Point3f(b.p_max.x, b.p_min.y, b.p_max.z)));
+	ret = bUnion(ret, M(Point3f(b.p_max.x, b.p_max.y, b.p_max.z)));
+	return ret;
+}
+
+CIVET_CPU_GPU
+Transform Transform::operator*(const Transform& t2) const {
+	return Transform(Matrix4::matmul(m, t2.m), Matrix4::matmul(t2.m_inv, m_inv));
+}
+
+CIVET_CPU_GPU
+bool Transform::swapsHandedness() const {
+	float det = m.m[0][0] * (m.m[1][1] * m.m[2][2] - m.m[1][2] * m.m[2][1]) -
+			m.m[0][1] * (m.m[1][0] * m.m[2][2] - m.m[1][2] * m.m[2][0]) +
+			m.m[0][2] * (m.m[1][0] * m.m[2][1] - m.m[1][1] * m.m[2][0]);
+	return det < 0;
+}
+
+} // namespace civet
---
+++ src/core/transform.h
@@ -0,0 +1,279 @@
+#ifndef CIVET_TRANSFORM_H
+#define CIVET_TRANSFORM_H
+
+#include "civet.h"
+#include "ray.h"
+#include "vecmath.h"
+
+namespace civet {
+
+struct Matrix4 {
+	CIVET_CPU_GPU
+	Matrix4() {
+		m[0][0] = m[1][1] = m[2][2] = m[3][3] = 1.0f;
+		m[0][1] = m[0][2] = m[0][3] = m[1][0] = m[1][2] = m[1][3] = m[2][0] =
+				m[2][1] = m[2][3] = m[3][0] = m[3][1] = m[3][2] = 0.0f;
+	}
+
+	CIVET_CPU_GPU
+	Matrix4(float mat[4][4]);
+
+	CIVET_CPU_GPU
+	Matrix4(float t00, float t01, float t02, float t03,
+			float t10, float t11, float t12, float t13,
+			float t20, float t21, float t22, float t23,
+			float t30, float t31, float t32, float t33);
+
+	CIVET_CPU_GPU
+	bool operator==(const Matrix4& m2) const {
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				if (m[i][j] != m2.m[i][j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	CIVET_CPU_GPU
+	bool operator!=(const Matrix4& m2) const {
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				if (m[i][j] != m2.m[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	CIVET_CPU_GPU
+	friend Matrix4 transpose(const Matrix4& mat);
+
+	void print(FILE* f) const {
+		fprintf(f, "[ ");
+		for (int i = 0; i < 4; ++i) {
+			fprintf(f, "  [ ");
+			for (int j = 0; j < 4; ++j) {
+				fprintf(f, "%f", m[i][j]);
+				if (j != 3) {
+					fprintf(f, ", ");
+				}
+			}
+			fprintf(f, " ]\n");
+		}
+		fprintf(f, " ] ");
+	}
+
+	CIVET_CPU_GPU
+	static Matrix4 matmul(const Matrix4& m1, const Matrix4& m2) {
+		Matrix4 r;
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				r.m[i][j] = m1.m[i][0] * m2.m[0][j] + m1.m[i][1] * m2.m[1][j] +
+						m1.m[i][2] * m2.m[2][j] + m1.m[i][3] * m2.m[3][j];
+			}
+		}
+		return r;
+	}
+
+	CIVET_CPU_GPU
+	friend Matrix4 inverse(const Matrix4& mat);
+
+	float m[4][4];
+};
+
+class Transform {
+public:
+	Transform() {}
+
+	CIVET_CPU_GPU
+	Transform(const float mat[4][4]) {
+		m = Matrix4(mat[0][0], mat[0][1], mat[0][2], mat[0][3],
+				mat[1][0], mat[1][1], mat[1][2], mat[1][3],
+				mat[2][0], mat[2][1], mat[2][2], mat[2][3],
+				mat[3][0], mat[3][1], mat[3][2], mat[3][3]);
+		m_inv = inverse(m);
+	}
+
+	CIVET_CPU_GPU
+	Transform(const Matrix4& mat) :
+			m(mat), m_inv(inverse(mat)) {}
+
+	CIVET_CPU_GPU
+	Transform(const Matrix4& mat, const Matrix4& mat_inv) :
+			m(mat), m_inv(mat_inv) {}
+
+	CIVET_CPU_GPU
+	friend Transform inverse(const Transform& t) {
+		return Transform(t.m_inv, t.m);
+	}
+
+	CIVET_CPU_GPU
+	friend Transform transpose(const Transform& t) {
+		return Transform(transpose(t.m), transpose(t.m_inv));
+	}
+
+	CIVET_CPU_GPU
+	bool operator==(const Transform& t) const {
+		return t.m == m && t.m_inv == m_inv;
+	}
+
+	CIVET_CPU_GPU
+	bool operator!=(const Transform& t) const {
+		return t.m != m || t.m_inv != m_inv;
+	}
+
+	CIVET_CPU_GPU
+	bool operator<(const Transform& t2) const {
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 4; ++j) {
+				if (m.m[i][j] < t2.m.m[i][j]) {
+					return true;
+				}
+				if (m.m[i][j] > t2.m.m[i][j]) {
+					return false;
+				}
+			}
+		}
+		return false;
+	}
+
+	CIVET_CPU_GPU
+	bool isIdentity() const {
+		return (m.m[0][0] == 1.f && m.m[0][1] == 0.f && m.m[0][2] == 0.f && m.m[0][3] == 0.f &&
+				m.m[1][0] == 0.f && m.m[1][1] == 1.f && m.m[1][2] == 0.f && m.m[1][3] == 0.f &&
+				m.m[2][0] == 0.f && m.m[2][1] == 0.f && m.m[2][2] == 1.f && m.m[2][3] == 0.f &&
+				m.m[3][0] == 0.f && m.m[3][1] == 0.f && m.m[3][2] == 0.f && m.m[3][3] == 1.f);
+	}
+
+	const Matrix4& getMatrix() const { return m; }
+	const Matrix4& getInverseMatrix() const { return m_inv; }
+
+	CIVET_CPU_GPU
+	bool hasScale() const {
+		float la2 = (*this)(Vector3f(1, 0, 0)).lengthSquared();
+		float lb2 = (*this)(Vector3f(0, 1, 0)).lengthSquared();
+		float lc2 = (*this)(Vector3f(0, 0, 1)).lengthSquared();
+#define NOT_ONE(x) ((x) < .999f || (x) > 1.001f)
+		return (NOT_ONE(la2) || NOT_ONE(lb2) || NOT_ONE(lc2));
+#undef NOT_ONE
+	}
+
+	// Apply transforms
+	template <typename T>
+	CIVET_CPU_GPU inline Point3<T> operator()(const Point3<T>& p) const;
+	template <typename T>
+	CIVET_CPU_GPU inline Vector3<T> operator()(const Vector3<T>& v) const;
+	template <typename T>
+	CIVET_CPU_GPU inline Normal3<T> operator()(const Normal3<T>&) const;
+	CIVET_CPU_GPU
+	inline Ray operator()(const Ray& r) const;
+	CIVET_CPU_GPU
+	inline RayDifferential operator()(const RayDifferential& r) const;
+
+	CIVET_CPU_GPU
+	Bounds3f operator()(const Bounds3f& b) const;
+	CIVET_CPU_GPU
+	Transform operator*(const Transform& t2) const;
+
+	CIVET_CPU_GPU
+	bool swapsHandedness() const;
+
+	// Apply transforms with rounding errors
+	template <typename T>
+	inline Point3<T> operator()(const Point3<T>& pt, Vector3<T>* absError) const;
+	template <typename T>
+	inline Point3<T> operator()(const Point3<T>& p, const Vector3<T>& pError, Vector3<T>* pTransError) const;
+	template <typename T>
+	inline Vector3<T> operator()(const Vector3<T>& v, Vector3<T>* vTransError) const;
+	template <typename T>
+	inline Vector3<T> operator()(const Vector3<T>& v, const Vector3<T>& vError, Vector3<T>* vTransError) const;
+	inline Ray operator()(const Ray& r, Vector3f* oError, Vector3f* dError) const;
+	inline Ray operator()(const Ray& r, const Vector3f& oErrorIn, const Vector3f& dErrorIn, Vector3f* oErrorOut, Vector3f* dErrorOut) const;
+
+
+
+private:
+	Matrix4 m, m_inv;
+};
+
+CIVET_CPU_GPU
+Transform translate(const Vector3f& delta);
+CIVET_CPU_GPU
+Transform scale(const float x, const float y, const float z);
+CIVET_CPU_GPU
+Transform rotateX(float theta);
+CIVET_CPU_GPU
+Transform rotateY(float theta);
+CIVET_CPU_GPU
+Transform rotateZ(float theta);
+CIVET_CPU_GPU
+Transform rotate(float theta, Vector3f& axis);
+CIVET_CPU_GPU
+Transform lookAt(const Point3f& position, const Point3f& target, const Vector3f& up);
+
+// Transform Inline Functions
+template <typename T>
+CIVET_CPU_GPU inline Point3<T> Transform::operator()(const Point3<T>& p) const {
+	T x = p.x, y = p.y, z = p.z;
+	T xp = m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z + m.m[0][3];
+	T yp = m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z + m.m[1][3];
+	T zp = m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z + m.m[2][3];
+	T wp = m.m[3][0] * x + m.m[3][1] * y + m.m[3][2] * z + m.m[3][3];
+	if (wp == 1) {
+		return Point3<T>(xp, yp, zp);
+	} else {
+		return Point3<T>(xp, yp, zp) / wp;
+	}
+}
+
+template <typename T>
+CIVET_CPU_GPU inline Vector3<T> Transform::operator()(const Vector3<T>& v) const {
+	T x = v.x, y = v.y, z = v.z;
+	return Vector3<T>(m.m[0][0] * x + m.m[0][1] * y + m.m[0][2] * z,
+			m.m[1][0] * x + m.m[1][1] * y + m.m[1][2] * z,
+			m.m[2][0] * x + m.m[2][1] * y + m.m[2][2] * z);
+}
+
+template <typename T>
+CIVET_CPU_GPU inline Normal3<T> Transform::operator()(const Normal3<T>& n) const {
+	T x = n.x, y = n.y, z = n.z;
+	return Normal3<T>(m_inv.m[0][0] * x + m_inv.m[1][0] * y + m_inv.m[2][0] * z,
+			m_inv.m[0][1] * x + m_inv.m[1][1] * y + m_inv.m[2][1] * z,
+			m_inv.m[0][2] * x + m_inv.m[1][2] * y + m_inv.m[2][2] * z);
+}
+
+CIVET_CPU_GPU
+inline Ray Transform::operator()(const Ray& r) const {
+	Vector3f oError;
+	Point3f o = (*this)(r.o, &oError);
+	Vector3f d = (*this)(r.d);
+
+	// Offset ray origin to edge of error bounds and compute t_max
+	float lengthSquared = d.lengthSquared();
+	float tMax = r.t_max;
+	if (lengthSquared > 0) {
+		float dt = dot(abs(d), oError) / lengthSquared;
+		o += d * dt;
+		tMax -= dt;
+	}
+	return Ray(o, d, tMax, r.time, r.medium);
+}
+
+CIVET_CPU_GPU
+inline RayDifferential Transform::operator()(const RayDifferential& r) const {
+	Ray tr = (*this)(Ray(r));
+	RayDifferential ret(tr.o, tr.d, tr.t_max, tr.time, tr.medium);
+	ret.has_differentials = r.has_differentials;
+	ret.ry_origin = (*this)(r.ry_origin);
+	ret.ry_origin = (*this)(r.ry_origin);
+	ret.ry_direction = (*this)(r.ry_direction);
+	ret.ry_direction = (*this)(r.ry_direction);
+	return ret;
+}
+
+} // namespace civet
+
+#endif // CIVET_TRANSFORM_H
