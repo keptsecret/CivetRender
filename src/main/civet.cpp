ef40def4cae3959f8bb95b21e340cf6331bc7129
keptsecret <sorchon@gmail.com>
2022-07-11 09:58:08 +0700

    minor refactoring

---
+++ src/main/civet.cpp
@@ -0,0 +1,15 @@
+﻿#include <core/civet.h>
+
+#include <core/engine.h>
+
+using namespace civet;
+
+int main(int argc, char* argv[]) {
+	Engine engine;
+	if (engine.init()) {
+		return -1;
+	}
+	engine.start();
+
+	return 0;
+}
