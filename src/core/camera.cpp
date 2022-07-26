8cdc7adbb7f08efa22f36ae77cd093694c6e0aca
keptsecret <sorchon@gmail.com>
2022-07-26 15:25:07 +0700

    added a live camera for OpenGL

--- src/core/camera.cpp
+++ src/core/camera.cpp
@@ -51,11 +51,62 @@ float Camera::generateRayDifferential(const CameraSample& sample, RayDifferentia
 ProjectiveCamera::ProjectiveCamera(const AnimatedTransform& ctw, const Transform& cts, const Bounds2f& screen_window,
 		float open, float close, float lensr, float focald, Film* f, const Medium* m) :
 		Camera(ctw, open, close, f, m), camera_to_screen(cts), lens_radius(lensr), focal_distance(focald) {
-	raster_to_camera = inverse(camera_to_screen) * raster_to_screen;
 	screen_to_raster = scale(film->full_resolution.x, film->full_resolution.y, 1) *
 			scale(1 / (screen_window.p_max.x - screen_window.p_min.x), 1 / (screen_window.p_max.y - screen_window.p_min.y), 1) *
 			translate(Vector3f(-screen_window.p_min.x, -screen_window.p_max.y, 0));
 	raster_to_screen = inverse(screen_to_raster);
+	raster_to_camera = inverse(camera_to_screen) * raster_to_screen;
+}
+
+GLCamera::GLCamera(Point3f pos, Vector3f up, float _yaw, float _pitch) :
+		position(pos), front(Vector3f(0, 0, -1)), world_up(up), yaw(_yaw), pitch(_pitch), mvmt_speed(2.5f), sensitivity(0.1f), zoom(45.0f) {
+	updateCameraVectors();
+}
+
+Transform GLCamera::getViewTransform() const {
+	return lookAtRH(position, position + front, up);
+}
+
+void GLCamera::translate(CameraMovement direction, float delta_time) {
+	float velocity = mvmt_speed * delta_time;
+	switch (direction) {
+		case FORWARD:
+			position += front * velocity;
+			break;
+		case BACKWARD:
+			position -= front * velocity;
+			break;
+		case LEFT:
+			position -= right * velocity;
+			break;
+		case RIGHT:
+			position += right * velocity;
+			break;
+	}
+}
+
+void GLCamera::pan(float xoffset, float yoffset, bool constrain_pitch) {
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (constrain_pitch) {
+		pitch = clamp(pitch, -89.0f, 89.0f);
+	}
+
+	updateCameraVectors();
+}
+
+void GLCamera::updateCameraVectors() {
+	Vector3f direction;
+	direction.x = std::cos(radians(yaw)) * std::cos(radians(pitch));
+	direction.y = std::sin(radians(pitch));
+	direction.z = std::sin(radians(yaw)) * std::cos(radians(pitch));
+	front = normalize(direction);
+	right = normalize(cross(front, world_up));
+	up = normalize(cross(right, front));
 }
 
 } // namespace civet
--- src/core/camera.h
+++ src/core/camera.h
@@ -2,6 +2,7 @@
 #define CIVET_CAMERA_H
 
 #include <core/civet.h>
+#include <core/film.h>
 #include <core/geometry/transform.h>
 
 namespace civet {
@@ -18,7 +19,7 @@ struct CameraSample {
 class Camera {
 public:
 	Camera(const AnimatedTransform& ctw, float open, float close, Film* f, const Medium* m);
-	virtual ~Camera();
+	virtual ~Camera() {}
 
 	virtual float generateRay(const CameraSample& sample, Ray* ray) const = 0;
 	virtual float generateRayDifferential(const CameraSample& sample, RayDifferential* rd) const;
@@ -38,6 +39,39 @@ protected:
 	Transform camera_to_screen, raster_to_camera;
 	Transform screen_to_raster, raster_to_screen;
 	float lens_radius, focal_distance;
+};
+
+enum CameraMovement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+class GLCamera {
+public:
+	GLCamera(Point3f pos = Point3f(0, 0, 0), Vector3f up = Vector3f(0, 1, 0), float _yaw = -90.0f, float _pitch = 0.0f);
+
+	Transform getViewTransform() const;
+
+	void translate(CameraMovement direction, float delta_time);
+	void pan(float xoffset, float yoffset, bool constrain_pitch = true);
+
+	Point3f position;
+	Vector3f front;
+	Vector3f up;
+	Vector3f right;
+	Vector3f world_up;
+
+	float yaw;
+	float pitch;
+
+	float mvmt_speed;
+	float sensitivity;
+	float zoom;
+
+private:
+	void updateCameraVectors();
 };
 
 } // namespace civet
