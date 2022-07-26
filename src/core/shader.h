a68283e0574bdf9ff740b4bcb1e81e81d30f4129
keptsecret <sorchon@gmail.com>
2022-07-26 15:31:01 +0700

    added shader class and basic matte shaders

---
+++ src/core/shader.h
@@ -0,0 +1,34 @@
+#ifndef CIVET_SHADER_H
+#define CIVET_SHADER_H
+
+#include <core/civet.h>
+#include <core/geometry/transform.h>
+#include <core/geometry/vecmath.h>
+#include <glad/glad.h>
+
+namespace civet {
+
+class Shader {
+public:
+	Shader(const char* vertex_path, const char* fragment_path);
+
+	void use();
+
+	void setBool(const std::string& name, bool value) const;
+	void setInt(const std::string& name, int value) const;
+	void setFloat(const std::string& name, float value) const;
+
+	void setVec3(const std::string& name, float x, float y, float z);
+	void setVec3(const std::string& name, const Vector3f& value);
+	void setVec4(const std::string& name, float x, float y, float z, float w);
+	void setMat4(const std::string& name, const Matrix4& value);
+
+	unsigned int ID;
+
+private:
+	void compileShader(const std::string& shader_path, const GLuint id);
+};
+
+} // namespace civet
+
+#endif // CIVET_SHADER_H
---
+++ src/material/matte_shader.frag
@@ -0,0 +1,8 @@
+#version 420 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+void main() {
+    FragColor = vec4(0.6, 0.6, 0.6, 1.0);
+}
