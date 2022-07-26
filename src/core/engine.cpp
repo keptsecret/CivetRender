2be94cf86a8b6fa740c40bbe65305b82d53db1b7
keptsecret <sorchon@gmail.com>
2022-07-26 16:09:56 +0700

    added a test scene and adjusted singleton

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -6,13 +6,55 @@ void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
 	glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow* window, GLCamera& camera, float delta_time) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		camera.translate(FORWARD, delta_time);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		camera.translate(BACKWARD, delta_time);
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		camera.translate(LEFT, delta_time);
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		camera.translate(RIGHT, delta_time);
+	}
+}
+
+void mouseCallback(GLFWwindow* window, double x_pos_in, double y_pos_in) {
+	float x_pos = float(x_pos_in);
+	float y_pos = float(y_pos_in);
+
+	Engine* e = Engine::getSingleton();
+	if (e->first_mouse) {
+		e->last_x = x_pos;
+		e->last_y = y_pos;
+		e->first_mouse = false;
+	}
+
+	float xoff = x_pos - e->last_x;
+	float yoff = e->last_y - y_pos;
+
+	if (e->invert_y) {
+		yoff = -yoff;
+	}
+
+	e->last_x = x_pos;
+	e->last_y = y_pos;
+	e->view_camera.pan(xoff, yoff);
+}
+
 int Engine::init() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow(800, 600, "New Window", nullptr, nullptr);
+	window = glfwCreateWindow(width, height, "New Window", nullptr, nullptr);
 	if (window == nullptr) {
 		printf("ERROR::Engine: Failed to create window.\n");
 		glfwTerminate();
@@ -24,29 +66,123 @@ int Engine::init() {
 		printf("ERROR::Engine: Failed to load GLAD.\n");
 		return -1;
 	}
-	glViewport(0, 0, 800, 600);
+	glViewport(0, 0, width, height);
 
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+	glfwSetCursorPosCallback(window, mouseCallback);
+
+	view_camera = GLCamera(Point3f(0, 0, 3));
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	return 0;
 }
 
 int Engine::start() {
+	/**
+	 * Test scene
+	 */
+	Shader shader("../civet/src/material/matte_shader.vert", "../civet/src/material/matte_shader.frag");
+	float vertices[] = {
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
+		0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
+		-0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
+		-0.5f, 0.5f, -0.5f, 0.0f, 1.0f
+	};
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glEnable(GL_DEPTH_TEST);
+
+	float last_frame = 0.0f;
+
 	while (!glfwWindowShouldClose(window)) {
+		float current_frame = float(glfwGetTime());
+		float delta_time = current_frame - last_frame;
+		last_frame = current_frame;
+
+		processInput(window, view_camera, delta_time);
+
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shader.use();
+		Transform projection = perspective(view_camera.zoom, width / height, 0.1f, 100.0f);
+		shader.setMat4("projection", projection.m);
+
+		Transform view = view_camera.getViewTransform();
+		shader.setMat4("view", view.m);
+
+		Transform model = translate(Vector3f(0, 0, 0));
+		shader.setMat4("model", model.m);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
 	glfwTerminate();
 	return 0;
 }
 
-Engine& Engine::getSingleton() {
+Engine* Engine::getSingleton() {
 	static Engine singleton;
-	return singleton;
+	return &singleton;
 }
 
 } // namespace civet
