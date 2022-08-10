0e97f2c8682b8f48b36d4d4795706a12d27800b2
keptsecret <sorchon@gmail.com>
2022-08-10 15:27:24 +0700

    added directional lights

--- src/shaders/simple_forward.h
+++ src/shaders/simple_forward.h
@@ -13,6 +13,17 @@ public:
 	void use(std::vector<Point3f>& light_positions) override {
 		Shader::use(light_positions);
 
+		// TODO: temporary hardcoded directional light
+		for (int i = 0; i < 4; i++) {
+			if (i == 0) {
+				setBool(("dirLights[" + std::to_string(i) + "].valid").c_str(), true);
+				setVec3(("dirLights[" + std::to_string(i) + "].direction").c_str(), Vector3f(2, 3, -1));
+				setVec3(("dirLights[" + std::to_string(i) + "].ambient").c_str(), 0.05f, 0.05f, 0.05f);
+				setVec3(("dirLights[" + std::to_string(i) + "].diffuse").c_str(), 0.8f, 0.8f, 0.8f);
+				setVec3(("dirLights[" + std::to_string(i) + "].specular").c_str(), 1.0f, 1.0f, 1.0f);
+			}
+		}
+
 		for (int i = 0; i < light_positions.size(); i++) {
 			setBool(("pointLights[" + std::to_string(i) + "].valid").c_str(), true);
 			setVec3(("pointLights[" + std::to_string(i) + "].position").c_str(), Vector3f(light_positions[i]));
--- src/shaders/simple_forward_frag.glsl
+++ src/shaders/simple_forward_frag.glsl
@@ -7,6 +7,15 @@ struct Material {
     float shininess;
 };
 
+struct DirLight {
+    bool valid;
+    vec3 direction;
+
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
 struct PointLight {
     bool valid;
     vec3 position;
@@ -20,6 +29,7 @@ struct PointLight {
     vec3 specular;
 };
 
+#define NUM_DIR_LIGHTS 4
 #define NUM_POINT_LIGHTS 32
 
 in vec3 FragPos;
@@ -27,9 +37,11 @@ in vec3 Normal;
 in vec2 TexCoords;
 
 uniform vec3 viewPos;
+uniform DirLight dirLights[NUM_DIR_LIGHTS];
 uniform PointLight pointLights[NUM_POINT_LIGHTS];
 uniform Material material;
 
+vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir);
 vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
 
 void main() {
@@ -37,8 +49,14 @@ void main() {
     vec3 viewDir = normalize(viewPos - FragPos);
 
     vec3 result = vec3(0,0,0);
+    for (int i = 0; i < NUM_DIR_LIGHTS; i++) {
+        if (dirLights[i].valid) {
+            result += calcDirLight(dirLights[i], normal, viewDir);
+        }
+    }
+
     for (int i = 0; i < NUM_POINT_LIGHTS; i++) {
-        if (pointLights[i].valid == true) {
+        if (pointLights[i].valid) {
             result += calcPointLight(pointLights[i], normal, FragPos, viewDir);
         }
     }
@@ -48,6 +66,22 @@ void main() {
     // manual gamma correction
     float gamma = 2.2;
     FragColor.rgb = pow(fragColor.rgb, vec3(1.0 / gamma));
+}
+
+vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir) {
+    vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
+
+    // diffuse shading
+    vec3 lightDir = normalize(-light.direction);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
+
+    // specular shading - using blinn-phong halfway vector
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), material.shininess);
+    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords));
+
+    return (ambient + diffuse + specular);
 }
 
 vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
