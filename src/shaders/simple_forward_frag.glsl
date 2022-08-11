66f053f0b2ef7a568343100610e3f05f6b5b2831
keptsecret <sorchon@gmail.com>
2022-08-11 11:37:33 +0700

    added shadow mapping techniques with pcf

--- src/shaders/simple_forward_frag.glsl
+++ src/shaders/simple_forward_frag.glsl
@@ -35,14 +35,17 @@ struct PointLight {
 in vec3 FragPos;
 in vec3 Normal;
 in vec2 TexCoords;
+in vec4 FragPosLightSpace;
 
 uniform vec3 viewPos;
 uniform DirLight dirLights[NUM_DIR_LIGHTS];
 uniform PointLight pointLights[NUM_POINT_LIGHTS];
 uniform Material material;
+uniform sampler2D shadowMap;
 
 vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir);
 vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+float calcShadow(vec4 fragPosLightSpace, vec3 normal, vec3 lightDir);
 
 void main() {
     vec3 normal = normalize(Normal);
@@ -81,7 +84,10 @@ vec3 calcDirLight(DirLight light, vec3 normal, vec3 viewDir) {
     float spec = pow(max(dot(normal, halfwayDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords));
 
-    return (ambient + diffuse + specular);
+    // shadow
+    float shadow = calcShadow(FragPosLightSpace, normal, lightDir);
+
+    return (ambient + (1.0 - shadow) * (diffuse + specular));
 }
 
 vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
@@ -105,5 +111,31 @@ vec3 calcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
     diffuse *= attenuation;
     specular *= attenuation;
 
-    return (ambient + diffuse + specular);
+    // shadow
+    float shadow = calcShadow(FragPosLightSpace, normal, lightDir);
+
+    return (ambient + (1.0 - shadow) * (diffuse + specular));
+}
+
+float calcShadow(vec4 fragPosLightSpace, vec3 normal, vec3 lightDir) {
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    projCoords = projCoords * 0.5 + 0.5;
+
+    float shadow = 0.0;
+    if (!(projCoords.z > 1.0)) {
+        float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
+        vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+        float currentDepth = projCoords.z;
+
+        // PCF, offset by 1 on each side
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r;
+                shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0f;
+            }
+        }
+        shadow /= 9.0;
+    }
+
+    return shadow;
 }
