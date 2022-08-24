6e3948eea0c8e30dd54ec6da9fe4ca25dc370a7a
keptsecret <sorchon@gmail.com>
2022-08-24 18:29:54 -0400

    added functions to send lights to shader

--- src/shaders/simple_forward.cpp
+++ src/shaders/simple_forward.cpp
@@ -2,4 +2,31 @@
 
 namespace civet {
 
+void SimpleForwardShader::setPointLights(std::vector<GLPointLight>& lights) {
+	for (int i = 0; i < lights.size(); i++) {
+		auto diffuse = lights[i].color * lights[i].intensity;
+
+		setBool(("pointLights[" + std::to_string(i) + "].valid").c_str(), lights[i].active && lights[i].cast_shadow);
+		setVec3(("pointLights[" + std::to_string(i) + "].position").c_str(), Vector3f(lights[i].position));
+		setVec3(("pointLights[" + std::to_string(i) + "].ambient").c_str(), diffuse * 0.1f);
+		setVec3(("pointLights[" + std::to_string(i) + "].diffuse").c_str(), diffuse);
+		setVec3(("pointLights[" + std::to_string(i) + "].specular").c_str(), 1.0f, 1.0f, 1.0f);
+		setFloat(("pointLights[" + std::to_string(i) + "].constant").c_str(), 1.0f);
+		setFloat(("pointLights[" + std::to_string(i) + "].linear").c_str(), 0.09f);
+		setFloat(("pointLights[" + std::to_string(i) + "].quadratic").c_str(), 0.032f);
+	}
+}
+
+void SimpleForwardShader::setDirectionalLights(std::vector<GLDirectionalLight>& lights) {
+	for (int i = 0; i < 4; i++) {
+		auto diffuse = lights[i].color * lights[i].intensity;
+
+		setBool(("dirLights[" + std::to_string(i) + "].valid").c_str(), lights[i].active && lights[i].cast_shadow);
+		setVec3(("dirLights[" + std::to_string(i) + "].direction").c_str(), lights[i].direction);
+		setVec3(("dirLights[" + std::to_string(i) + "].ambient").c_str(), diffuse * 0.1f);
+		setVec3(("dirLights[" + std::to_string(i) + "].diffuse").c_str(), diffuse);
+		setVec3(("dirLights[" + std::to_string(i) + "].specular").c_str(), 1.0f, 1.0f, 1.0f);
+	}
+}
+
 } // namespace civet
--- src/shaders/simple_forward.h
+++ src/shaders/simple_forward.h
@@ -2,6 +2,7 @@
 #define CIVET_SIMPLE_FORWARD_SHADER_H
 
 #include <core/shader.h>
+#include <core/light.h>
 
 namespace civet {
 
@@ -10,31 +11,8 @@ public:
 	SimpleForwardShader() :
 			Shader("../civet/src/shaders/simple_forward_vert.glsl", "../civet/src/shaders/simple_forward_frag.glsl") {}
 
-	void use(std::vector<Point3f>& light_positions) override {
-		Shader::use(light_positions);
-
-		// TODO: temporary hardcoded directional light
-		for (int i = 0; i < 4; i++) {
-			if (i == 0) {
-				setBool(("dirLights[" + std::to_string(i) + "].valid").c_str(), true);
-				setVec3(("dirLights[" + std::to_string(i) + "].direction").c_str(), Vector3f(2, -1, -1));
-				setVec3(("dirLights[" + std::to_string(i) + "].ambient").c_str(), 0.05f, 0.05f, 0.05f);
-				setVec3(("dirLights[" + std::to_string(i) + "].diffuse").c_str(), 0.8f, 0.8f, 0.8f);
-				setVec3(("dirLights[" + std::to_string(i) + "].specular").c_str(), 1.0f, 1.0f, 1.0f);
-			}
-		}
-
-		for (int i = 0; i < light_positions.size(); i++) {
-			setBool(("pointLights[" + std::to_string(i) + "].valid").c_str(), true);
-			setVec3(("pointLights[" + std::to_string(i) + "].position").c_str(), Vector3f(light_positions[i]));
-			setVec3(("pointLights[" + std::to_string(i) + "].ambient").c_str(), 0.05f, 0.05f, 0.05f);
-			setVec3(("pointLights[" + std::to_string(i) + "].diffuse").c_str(), 0.8f, 0.8f, 0.8f);
-			setVec3(("pointLights[" + std::to_string(i) + "].specular").c_str(), 1.0f, 1.0f, 1.0f);
-			setFloat(("pointLights[" + std::to_string(i) + "].constant").c_str(), 1.0f);
-			setFloat(("pointLights[" + std::to_string(i) + "].linear").c_str(), 0.09f);
-			setFloat(("pointLights[" + std::to_string(i) + "].quadratic").c_str(), 0.032f);
-		}
-	}
+	void setPointLights(std::vector<GLPointLight>& lights);
+	void setDirectionalLights(std::vector<GLDirectionalLight>& lights);
 };
 
 } // namespace civet
