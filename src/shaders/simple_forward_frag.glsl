c1d70abb8cdc5136bfdd6834603cc55e77fc1fe1
keptsecret <sorchon@gmail.com>
2022-08-27 18:00:06 -0400

    added pcf for point light shadows

--- src/shaders/simple_forward_frag.glsl
+++ src/shaders/simple_forward_frag.glsl
@@ -31,6 +31,7 @@ struct PointLight {
     vec3 diffuse;
     vec3 specular;
 
+    float far_plane;
     samplerCube shadow_map;
 };
 
@@ -137,27 +138,46 @@ float calcShadow(DirLight light, vec4 fragPosLightSpace, vec3 normal, vec3 light
         vec2 texelSize = 1.0 / textureSize(light.shadow_map, 0);
         float currentDepth = projCoords.z;
 
-        // PCF, offset by 1 on each side
-        for (int x = -1; x <= 1; x++) {
-            for (int y = -1; y <= 1; y++) {
+        // PCF, offset by 2 on each side
+        for (int x = -2; x <= 2; x++) {
+            for (int y = -2; y <= 2; y++) {
                 float pcfDepth = texture(light.shadow_map, projCoords.xy + vec2(x, y) * texelSize).r;
                 shadow += currentDepth - bias > pcfDepth ? 1.0 : 0.0f;
             }
         }
-        shadow /= 9.0;
+        shadow /= 25.0;
     }
 
     return shadow;
 }
 
+vec3 sampleOffsetDirections[20] = vec3[]
+(
+vec3( 1,  1,  1), vec3( 1, -1,  1), vec3(-1, -1,  1), vec3(-1,  1,  1),
+vec3( 1,  1, -1), vec3( 1, -1, -1), vec3(-1, -1, -1), vec3(-1,  1, -1),
+vec3( 1,  1,  0), vec3( 1, -1,  0), vec3(-1, -1,  0), vec3(-1,  1,  0),
+vec3( 1,  0,  1), vec3(-1,  0,  1), vec3( 1,  0, -1), vec3(-1,  0, -1),
+vec3( 0,  1,  1), vec3( 0, -1,  1), vec3( 0, -1, -1), vec3( 0,  1, -1)
+);
+
 float calcShadowCube(PointLight light, vec3 fragPos, vec3 lightPos) {
     vec3 fragToLight = fragPos - lightPos;
-    float closestDepth = texture(light.shadow_map, fragToLight).r;
-    closestDepth *= 25.0;
 
     float currentDepth = length(fragToLight);
-    float bias = 0.05;
-    float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+    float shadow = 0.0;
+    float bias = 0.15;
+    int samples = 20;
+    float viewDistance = length(viewPos - fragPos);
+    float radius = 0.05;
+
+    for (int i = 0; i < samples; i++) {
+        float closestDepth = texture(light.shadow_map, fragToLight + sampleOffsetDirections[i] * radius).r;
+        closestDepth *= light.far_plane;
+        if (currentDepth - bias > closestDepth) {
+            shadow += 1.0;
+        }
+    }
+    shadow /= float(samples);
 
     return shadow;
 }
