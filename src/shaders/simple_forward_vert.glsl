1a2491667e53670e39ac5f5358d36b6d8c89781f
keptsecret <sorchon@gmail.com>
2022-08-28 17:30:09 -0400

    added shader support for normal maps

--- src/shaders/simple_forward_vert.glsl
+++ src/shaders/simple_forward_vert.glsl
@@ -2,10 +2,12 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 layout (location = 2) in vec2 aTexCoords;
+layout (location = 3) in vec3 aTangent;
 
 out vec3 FragPos;
 out vec3 Normal;
 out vec2 TexCoords;
+out mat3 TBN;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -16,4 +18,11 @@ void main() {
     FragPos = vec3(model * vec4(aPos, 1.0));
     Normal = aNormal;
     TexCoords = aTexCoords;
+
+    vec3 T = normalize(vec3(model * vec4(aTangent, 0.0)));
+    vec3 N = normalize(vec3(model * vec4(aNormal, 0.0)));
+
+    T = normalize(T - dot(T, N) * N);
+    vec3 B = cross(N, T);
+    TBN = transpose(mat3(T, B, N));
 }
