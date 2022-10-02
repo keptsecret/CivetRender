ed8b38f736bd359f8026a55628b86d99fdb6594c
keptsecret <sorchon@gmail.com>
2022-10-02 15:30:48 -0400

    formatting changes

--- src/materials/dielectric.h
+++ src/materials/dielectric.h
@@ -11,7 +11,7 @@ public:
 			const std::shared_ptr<Texture<float>>& roughness_, const std::shared_ptr<Texture<float>>& bump_map_, bool remap_roughness_) :
 			Kd(Kd_), Ks(Ks_), roughness(roughness_), bump_map(bump_map_), remap_roughness(remap_roughness_) {}
 
-	void computeScatteringFunctions(SurfaceInteraction *isect, MemoryArena &arena, TransportMode mode, bool allow_multiple_lobes) const override;
+	void computeScatteringFunctions(SurfaceInteraction* isect, MemoryArena& arena, TransportMode mode, bool allow_multiple_lobes) const override;
 
 private:
 	std::shared_ptr<Texture<Spectrum>> Kd, Ks;
