ec0021d17f95e8034b12118fc39c9cefd71d4a90
keptsecret <sorchon@gmail.com>
2023-05-08 15:49:57 +0700

    reorganized scene tree panel code

--- src/core/editor.cpp
+++ src/core/editor.cpp
@@ -18,11 +18,11 @@ void Editor::draw(Scene& active_scene) {
 	ImGui::SetNextWindowSize(ImVec2(300, 400));
 	inspector(active_scene);
 
-//	ImGui::Begin("Debug");
-//	ImGui::Text("Camera pitch: %.3f, yaw: %.3f", view_camera.pitch, view_camera.yaw);
-//	ImGui::Text("Camera front x: %.3f, y: %.3f, z: %.3f", view_camera.front.x, view_camera.front.y, view_camera.front.z);
-//	ImGui::Text("Camera up x: %.3f, y: %.3f, z: %.3f", view_camera.up.x, view_camera.up.y, view_camera.up.z);
-//	ImGui::End();
+	//	ImGui::Begin("Debug");
+	//	ImGui::Text("Camera pitch: %.3f, yaw: %.3f", view_camera.pitch, view_camera.yaw);
+	//	ImGui::Text("Camera front x: %.3f, y: %.3f, z: %.3f", view_camera.front.x, view_camera.front.y, view_camera.front.z);
+	//	ImGui::Text("Camera up x: %.3f, y: %.3f, z: %.3f", view_camera.up.x, view_camera.up.y, view_camera.up.z);
+	//	ImGui::End();
 
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData()); ///< here as well
@@ -31,47 +31,56 @@ void Editor::draw(Scene& active_scene) {
 void Editor::sceneTree(Scene& active_scene) {
 	ImGui::Begin("Scene Tree", &show_scene_tree, ImGuiWindowFlags_HorizontalScrollbar);
 
-	if (ImGui::TreeNode("Scene")) {
-		ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, ImGui::GetFontSize() * 3);
-		for (int i = 0; i < active_scene.nodes.size(); i++) {
-			ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_OpenOnArrow | ImGuiTreeNodeFlags_OpenOnDoubleClick;
-			// only models have children for now
-			if (active_scene.nodes[i]->type != Model) {
-				node_flags = ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
-				ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, "%s", active_scene.nodes[i]->name.c_str());
-			} else {
-				bool node_open = ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, "%s", active_scene.nodes[i]->name.c_str());
+	if (ImGui::TreeNodeEx("Scene", ImGuiTreeNodeFlags_DefaultOpen)) {
+		ImGui::PushStyleVar(ImGuiStyleVar_IndentSpacing, ImGui::GetFontSize() * 1.5);
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0.0f, 0.0f });
+		ImGui::PushStyleVar(ImGuiStyleVar_ItemInnerSpacing, ImVec2{ 3.0f, 3.0f });
 
-				sceneTreeNode(active_scene, node_flags, node_open, i);
-			}
-			if (ImGui::IsItemHovered(ImGuiHoveredFlags_AllowWhenBlockedByPopup) && ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
-				active_scene.selected_node = active_scene.nodes[i];
-				show_inspector = true;
+		for (int i = 0; i < active_scene.nodes.size(); i++) {
+			auto node = active_scene.nodes[i];
+			TreeNodeState state = sceneTreeNode(active_scene, node);
+
+			if (state.is_open) {
+				if (node->type == Model) {
+					auto model = std::static_pointer_cast<GLModel>(node);
+					auto meshes = model->getMeshes();
+					for (int j = 0; j < meshes.size(); j++) {
+						sceneTreeNode(active_scene, meshes[j]);
+					}
+				}
+
+				if (state.should_pop) {
+					ImGui::TreePop();
+				}
 			}
 		}
+
 		ImGui::TreePop();
-		ImGui::PopStyleVar();
+		ImGui::PopStyleVar(3);
 	}
 
 	ImGui::End();
 }
 
-void Editor::sceneTreeNode(Scene& active_scene, ImGuiTreeNodeFlags node_flags, bool node_open, int index) {
-	if (node_open) {
-		auto n = active_scene.nodes[index];
-		switch (n->type) {
-			case Model:
-				auto m = std::static_pointer_cast<GLModel>(n);
-				auto meshes = m->getMeshes();
-				for (int i = 0; i < meshes.size(); i++) {
-					// meshes are leaf nodes, for now
-					node_flags |= ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen;
-					ImGui::TreeNodeEx((void*)(intptr_t)i, node_flags, "%s", meshes[i].name.c_str());
-				}
-				break;
-		}
-		ImGui::TreePop();
+TreeNodeState Editor::sceneTreeNode(Scene& active_scene, std::shared_ptr<Node> node) {
+	bool is_leaf = node->type != Model;
+
+	ImGuiTreeNodeFlags node_flags = ImGuiTreeNodeFlags_SpanAvailWidth |
+			(is_leaf
+							? (ImGuiTreeNodeFlags_Leaf | ImGuiTreeNodeFlags_NoTreePushOnOpen)
+							: ImGuiTreeNodeFlags_OpenOnArrow) |
+			(active_scene.selected_node == node
+							? ImGuiTreeNodeFlags_Selected
+							: ImGuiTreeNodeFlags_None);
+
+	bool node_open = ImGui::TreeNodeEx(node->name.c_str(), node_flags);
+
+	if (ImGui::IsItemHovered(ImGuiHoveredFlags_AllowWhenBlockedByPopup) && ImGui::IsMouseReleased(ImGuiMouseButton_Left)) {
+		active_scene.selected_node = node;
+		show_inspector = true;
 	}
+
+	return TreeNodeState{ node_open, !is_leaf };
 }
 
 void Editor::inspector(Scene& active_scene) {
