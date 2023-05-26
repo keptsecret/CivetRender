94589409281babdd7bcb953d8b61c5f4115aeece
keptsecret <sorchon@gmail.com>
2023-05-26 10:30:21 +0700

    build scene

--- src/core/scene.cpp
+++ src/core/scene.cpp
@@ -1,12 +1,20 @@
 #include <core/scene.h>
 
+#include <lights/distant.h>
+#include <lights/point.h>
+#include <materials/disney.h>
+#include <shapes/triangle.h>
+#include <textures/constant.h>
+#include <textures/imagemap.h>
+#include <utils/bvh.h>
+
 #include <assimp/postprocess.h>
 #include <assimp/Importer.hpp>
 
 namespace civet {
 
 Scene::Scene(const char* path) {
-	SkyboxParameters params{ Vector3f(0.f, 0.2f, 0.5f), 512, 4 };
+	SkyboxParameters params{ Vector3f(0.f, 0.2f, 0.5f), 128, 4 };
 	skybox = std::make_shared<Skybox>();
 	skybox->init(params);
 	nodes.push_back(skybox);
@@ -72,6 +80,140 @@ void Scene::addNode(std::shared_ptr<Node> node, NodeType type) {
 	}
 
 	nodes.push_back(node);
+}
+
+void Scene::buildScene() {
+	printf("Building scene for ray tracing...");
+	std::vector<std::shared_ptr<Primitive>> prims;
+	for (const auto model : models) {
+		auto meshes = model->getMeshes();
+		for (const auto mesh : meshes) {
+			// Convert mesh data into shapes
+			std::vector<int> indices(mesh->indices.begin(), mesh->indices.end());
+
+			std::vector<Point3f> vertices(mesh->vertices.size());
+			std::vector<Normal3f> normals(mesh->vertices.size());
+			std::vector<Vector3f> tangents(mesh->vertices.size());
+			std::vector<Point2f> uvs(mesh->vertices.size());
+
+			for (const auto tri : mesh->vertices) {
+				vertices.push_back(tri.position);
+				normals.push_back(tri.normal);
+				tangents.push_back(tri.tangent);
+				uvs.push_back(tri.uv);
+			}
+
+			auto trimesh = std::make_shared<TriangleMesh>(mesh->getWorldTransform(), indices.size(), indices.data(),
+					vertices.size(), vertices.data(), tangents.data(), normals.data(), uvs.data(), nullptr);
+			int n_tris = indices.size();
+			std::vector<std::shared_ptr<Shape>> tris;
+			tris.reserve(n_tris);
+			for (int i = 0; i < n_tris; i++) {
+				tris.push_back(std::make_shared<Triangle>(&mesh->transform_data.transform, nullptr, false, trimesh, i)); // TODO: could cause problems
+			}
+
+			// Convert material
+			///< we only have Disney BRDF so far in glsl
+			///< doesn't map over well and is missing lots of textures, including normal maps
+			auto material = mesh->material;
+			DisneyBSDFParams params;
+			for (const auto tex : material->textures) {
+				std::unique_ptr<UVMapping2D> map;
+				map.reset(new UVMapping2D(1., 1., 0., 0.));
+				if (tex->type == "texture_albedo") {
+					auto texture = new ImageTexture<RGBSpectrum, Spectrum>(std::move(map), tex->path, false, 8.f, ImageWrap::Repeat, 1.f, true);
+					params.color = std::shared_ptr<Texture<Spectrum>>(texture);
+				} else if (tex->type == "texture_metallic") {
+					auto texture = new ImageTexture<RGBSpectrum, float>(std::move(map), tex->path, false, 8.f, ImageWrap::Repeat, 1.f, true);
+					params.metallic = std::shared_ptr<Texture<float>>(texture);
+				} else if (tex->type == "texture_roughness") {
+					auto texture = new ImageTexture<RGBSpectrum, float>(std::move(map), tex->path, false, 8.f, ImageWrap::Repeat, 1.f, true);
+					params.roughness = std::shared_ptr<Texture<float>>(texture);
+				} else if (tex->type == "texture_ao") {
+					auto texture = new ImageTexture<RGBSpectrum, float>(std::move(map), tex->path, false, 8.f, ImageWrap::Repeat, 1.f, true);
+					params.metallic = std::shared_ptr<Texture<float>>(texture);
+				} else if (tex->type == "texture_bump") {
+					auto texture = new ImageTexture<RGBSpectrum, float>(std::move(map), tex->path, false, 8.f, ImageWrap::Repeat, 1.f, true);
+					params.bump = std::shared_ptr<Texture<float>>(texture);
+				}
+			}
+
+			std::shared_ptr<Texture<Spectrum>> color = params.color ? params.color : std::make_shared<ConstantTexture<Spectrum>>(Spectrum(0.5f));
+			std::shared_ptr<Texture<float>> metallic = params.metallic ? params.metallic : std::make_shared<ConstantTexture<float>>(material->metallic);
+			std::shared_ptr<Texture<float>> eta = params.eta ? params.eta : std::make_shared<ConstantTexture<float>>(1.5f);
+			std::shared_ptr<Texture<float>> roughness = params.metallic ? params.metallic : std::make_shared<ConstantTexture<float>>(material->roughness);
+			std::shared_ptr<Texture<float>> spec_tint = params.spec_tint ? params.spec_tint : std::make_shared<ConstantTexture<float>>(0.f);
+			std::shared_ptr<Texture<float>> anisotropic = params.anis ? params.anis : std::make_shared<ConstantTexture<float>>(0.f);
+			std::shared_ptr<Texture<float>> sheen = params.sheen ? params.sheen : std::make_shared<ConstantTexture<float>>(0.f);
+			std::shared_ptr<Texture<float>> sheen_tint = params.sheen_tint ? params.sheen_tint : std::make_shared<ConstantTexture<float>>(0.5f);
+			std::shared_ptr<Texture<float>> clearcoat = params.cc ? params.cc : std::make_shared<ConstantTexture<float>>(0.f);
+			std::shared_ptr<Texture<float>> clearcoat_gloss = params.cc_gloss ? params.cc_gloss : std::make_shared<ConstantTexture<float>>(1.f);
+			std::shared_ptr<Texture<float>> spec_trans = params.spec_trans ? params.spec_trans : std::make_shared<ConstantTexture<float>>(0.f);
+			std::shared_ptr<Texture<Spectrum>> scatter_dist = params.scatter_dist ? params.scatter_dist : std::make_shared<ConstantTexture<Spectrum>>(Spectrum(0.f));
+			bool thin = params.t;
+			std::shared_ptr<Texture<float>> flatness = params.flatness ? params.flatness : std::make_shared<ConstantTexture<float>>(0.f);
+			std::shared_ptr<Texture<float>> diff_trans = params.diff_trans ? params.diff_trans : std::make_shared<ConstantTexture<float>>(1.f);
+			std::shared_ptr<Texture<float>> bump = params.bump;
+			auto mapped_mat = std::make_shared<DisneyMaterial>(color, metallic, eta, roughness, spec_tint,
+					anisotropic, sheen, sheen_tint, clearcoat,
+					clearcoat_gloss, spec_trans, scatter_dist, thin,
+					flatness, diff_trans, bump);
+
+			for (auto& t : tris) {
+				prims.push_back(std::make_shared<GeometricPrimitive>(t, mapped_mat, nullptr, nullptr));
+			}
+		}
+	}
+
+	printf("Building BVH...");
+	aggregate = std::make_shared<BVH>(prims, 0, BVH::SplitMethod::SAH);
+	world_bound = aggregate->worldBound();
+
+	// Convert lights
+	printf("Building lights...");
+	for (const auto& dir : dir_lights) {
+		RGBSpectrum rgb;
+		rgb[0] = dir->color.x * dir->power;
+		rgb[1] = dir->color.y * dir->power;
+		rgb[2] = dir->color.z * dir->power;
+		lights.push_back(std::make_shared<DistantLight>(dir->transform_data.transform, rgb, dir->direction));
+	}
+	for (const auto& point : point_lights) {
+		RGBSpectrum rgb;
+		rgb[0] = point->color.x * point->power;
+		rgb[1] = point->color.y * point->power;
+		rgb[2] = point->color.z * point->power;
+		lights.push_back(std::make_shared<PointLight>(point->transform_data.transform, MediumInterface(), point->position, rgb));
+	}
+
+	for (const auto& light : lights) {
+		light->preprocess(*this);
+	}
+}
+
+bool Scene::intersect(const Ray& ray, SurfaceInteraction* isect) const {
+	return aggregate->intersect(ray, isect);
+}
+
+bool Scene::intersectP(const Ray& ray) const {
+	return aggregate->intersectP(ray);
+}
+
+bool Scene::intersectTr(Ray ray, Sampler& sampler, SurfaceInteraction* isect, Spectrum* transmittance) const {
+	*transmittance = Spectrum(1.f);
+	while (true) {
+		bool hit = intersect(ray, isect);
+		if (ray.medium) {
+			*transmittance = ray.medium->Tr(ray, sampler);
+		}
+		if (!hit) {
+			return false;
+		}
+		if (isect->primitive->getMaterial() != nullptr) {
+			return true;
+		}
+		ray = isect->spawnRay(ray.d);
+	}
 }
 
 } // namespace civet
--- src/core/scene.h
+++ src/core/scene.h
@@ -2,10 +2,8 @@
 #define CIVET_SCENE_H
 
 #include <core/civet.h>
-#include <core/light.h>
-#include <core/mesh.h>
-
 #include <core/skybox.h>
+#include <map>
 
 namespace civet {
 
@@ -22,19 +20,33 @@ public:
 
 	void clearSelectedNode() { selected_node = nullptr; }
 
+	// ray tracing functions
+	const Bounds3f& worldBound() const { return world_bound; }
+	void buildScene();
+	bool intersect(const Ray& ray, SurfaceInteraction* isect) const;
+	bool intersectP(const Ray& ray) const;
+	bool intersectTr(Ray ray, Sampler& sampler, SurfaceInteraction* isect, Spectrum* transmittance) const;
+
 public:
 	std::vector<std::shared_ptr<GLModel>> models;
 	std::vector<std::shared_ptr<GLDirectionalLight>> dir_lights;
 	std::vector<std::shared_ptr<GLPointLight>> point_lights;
 
 	std::vector<std::shared_ptr<Node>> nodes;
-
 	std::shared_ptr<Skybox> skybox;
+
+	std::vector<std::shared_ptr<Light>> lights;
 
 private:
 	friend Editor;
 
 	std::shared_ptr<Node> selected_node = nullptr;
+
+	// ray tracing variables
+	std::shared_ptr<Primitive> aggregate;
+	// std::map<std::string, std::shared_ptr<Texture<float>>> float_textures;
+	// std::map<std::string, std::shared_ptr<Texture<Spectrum>>> spectrum_textures;
+	Bounds3f world_bound;
 };
 
 } // namespace civet
